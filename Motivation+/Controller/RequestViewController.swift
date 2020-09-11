454ff562e849c167f90c635b548dc8e7159943f9
yukendev <yuken0906dev@gmail.com>
2020-09-12 08:52:46 +0900

    request screen

--- Motivation+/Controller/RequestViewController.swift
+++ Motivation+/Controller/RequestViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class RequestViewController: UIViewController {
+class RequestViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let nameArray: [String] = ["増山", "春", "けんちゃん", "はるぴー"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "RequestTableViewCell", bundle: nil), forCellReuseIdentifier: "Request")
 
     }
     
 
     @IBAction func backAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Request", for: indexPath) as! RequestTableViewCell
+        
+        cell.selectionStyle = .none
+        
+        cell.nameLabel.text = nameArray[indexPath.row]
+        
+        return cell
     }
     
 
