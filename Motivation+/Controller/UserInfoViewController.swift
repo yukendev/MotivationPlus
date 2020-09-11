08f89aaed8e6df673cfa20ea36dc5614a8417a62
yukendev <yuken0906dev@gmail.com>
2020-09-11 20:40:27 +0900

    userInfo

--- Motivation+/Controller/UserInfoViewController.swift
+++ Motivation+/Controller/UserInfoViewController.swift
@@ -8,23 +8,54 @@
 
 import UIKit
 
-class UserInfoViewController: UIViewController {
+class UserInfoViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var editButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+        imageView.layer.cornerRadius = 20
+        imageView.isUserInteractionEnabled = true
+        editButton.layer.cornerRadius = 5
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
-    */
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    
+    
+    @IBAction func editAction(_ sender: Any) {
+        print("編集されました")
+    }
+    
+    @IBAction func pictureAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = .photoLibrary
+            cameraPicker.delegate = self
+            cameraPicker.allowsEditing = true
+            self.present(cameraPicker, animated: true, completion: nil)
+        }else{
+            print("error")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+        self.dismiss(animated: true)
+    }
+    
 }
