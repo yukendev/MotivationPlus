d8647e3cf8740755ddfaef2d70a6de85b3c0b8ea
yukendev <yuken0906dev@gmail.com>
2020-09-11 09:13:44 +0900

    Initial Commit

---
+++ Motivation+/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Motivation+
+//
+//  Created by 手塚友健 on 2020/09/11.
+//  Copyright © 2020 手塚友健. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
