01719298a27d4f1c01d544fe717b6400359269c2
yukendev <yuken0906dev@gmail.com>
2020-09-12 09:15:11 +0900

    search screen

--- Motivation+/Controller/SearchViewController.swift
+++ Motivation+/Controller/SearchViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+     let nameArray: [String] = ["増山", "春", "けんちゃん", "はるぴー"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "SearchTableViewCell", bundle: nil), forCellReuseIdentifier: "Search")
 
     }
     
 
     @IBAction func backAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Search", for: indexPath) as! SearchTableViewCell
+        
+        cell.nameLabel.text = nameArray[indexPath.row]
+        cell.selectionStyle = .none
+        
+        return cell
     }
     
 
